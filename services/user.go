f8672210212e4c5217888f3b7de15b1bb7afaaeb
Naveen Kumar <naveen.k@bookmyshow.com>
2022-01-15 23:34:08 +0530

    added get functionality for user

--- services/user.go
+++ services/user.go
@@ -5,9 +5,21 @@ import (
 	"go-self/go-mysql-api/utils/errors"
 )
 
-func CreateUser(user *user.User) (*user.User, *errors.RestErr) {
+func CreateUser(user user.User) (*user.User, *errors.RestErr) {
 	if err := user.Validate(); err != nil {
 		return nil, err
 	}
-	return user, nil
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func GetUser(userID int64) (*user.User, *errors.RestErr) {
+	var user user.User
+	user.Id = userID
+	if err := user.Get(); err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
