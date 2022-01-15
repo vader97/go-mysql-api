9cc38a8f790fd900ed6a741242d6ef14af9bc8ce
Naveen Kumar <naveen.k@bookmyshow.com>
2022-01-15 23:29:19 +0530

    added new error

--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -17,3 +17,11 @@ func NewBadRequestErr(message string) *RestErr {
 		Error:   "bad_request",
 	}
 }
+
+func NewNotFoundErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "bad_request",
+	}
+}
